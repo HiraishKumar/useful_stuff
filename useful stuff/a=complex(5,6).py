aa9d115105bfafec1d4d9d3e69b9148235bf920f
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-07-19 20:45:02 +0530

    stuff

--- useful stuff/a=complex(5,6).py
+++ useful stuff/a=complex(5,6).py
@@ -1,11 +1,13 @@
 a=input('input data: ')
 match type(a):
-  case type[str]:
-    print('it is a STRING')
+
   case type(int(a)):
     print('it is an INTEGER')  
   case type(float(a)):
     print('it is a FLOAT')
+  case type:
+    [str]
+    print('it is a STRING')
   
     
   
