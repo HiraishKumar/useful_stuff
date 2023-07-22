73e943399b6dbbc8ff9db2aa39365af3babe8401
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-07-22 22:54:46 +0530

    move

--- useful stuff/experimental.py
+++ useful stuff/experimental.py
@@ -38,3 +38,29 @@ for item in product:
 if mod<fin:
     mod = fin        
 print(fin)
+
+
+
+
+t=int(input('input integer here: ')) #This must be an odd number
+c = 'H'
+#Top Cone
+for i in range (0,t):
+    print((c*(1+2*i)).center(t*2,'-'))    
+#Top Pillars
+for i in range(t+1):
+    print((c*t).center(t*2,'-')+(c*t).center(t*6,'-'))
+#Middle Beam
+for i in range((t+1)//2):
+    print((c*(t*5)).center(t*6,'-'))
+#Bottom Pillars
+for i in range(t+1):
+    print((c*t).center(t*2,'-')+(c*t).center(t*6),'-')
+#Bottom cone
+for i in range(0,t):
+    print((c*(t*2-1-2*i)).center(t*10),'-')
+
+    
+    
+    
+
