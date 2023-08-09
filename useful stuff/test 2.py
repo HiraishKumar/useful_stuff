042c06d9b360af36f6844005fd0116c1e912a111
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-08-09 16:32:35 +0530

    testing

--- useful stuff/test 2.py
+++ useful stuff/test 2.py
@@ -1,17 +1,16 @@
-s=input('enter numeral ').upper()
-def fun(s):
-    d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-    sum=0
-    for i in range (len(s)):
-        if i!=(len(s)-1):
-            if d[s[i]]<d[s[i+1]]:
-                sum+= -d[s[i]]               
-            else:
-                sum+= d[s[i]]
-        else:
-            sum+= d[s[i]]
+# def isAnagram(s: str, t: str) -> bool:
+#     if len(s) != len(t):
+#         return False
+#     sorted_s = sorted(s)
+#     sorted_t = sorted(t)
+    
+#     return sorted_s == sorted_t  
 
-    return sum
-print(fun(s))
-
-
+def isAnagram( s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    s2= t[::-1]                
+    return s == s2
+        
+        
+print(isAnagram('anagram','nagaram'))
