cc543635e0c57dafd21c5fa9aff27d75a727c988
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-08-12 15:38:38 +0530

    testing

--- useful stuff/test 2.py
+++ useful stuff/test 2.py
@@ -1,16 +1,6 @@
-# def isAnagram(s: str, t: str) -> bool:
-#     if len(s) != len(t):
-#         return False
-#     sorted_s = sorted(s)
-#     sorted_t = sorted(t)
-    
-#     return sorted_s == sorted_t  
+for i in range(9):
+    print(i//3)
 
-def isAnagram( s: str, t: str) -> bool:
-    if len(s) != len(t):
-        return False
-    s2= t[::-1]                
-    return s == s2
-        
-        
-print(isAnagram('anagram','nagaram'))
+
+
+    
