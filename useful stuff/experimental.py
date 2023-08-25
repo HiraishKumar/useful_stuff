408f4fd17c9e1692a3e93e47379155b882ce2774
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-08-25 15:59:17 +0530

    stuff

--- useful stuff/experimental.py
+++ useful stuff/experimental.py
@@ -1,49 +1,21 @@
-# k,m=(input().split())
-# lst=[]
-# for i in range(int(k)):
-#     a=input().split()
-#     tst=[]
-#     for j in range(int(a[0])):
-#         tst.append(int(a[j+1]))
-#     print(tst)    
-#     lst.append(max(tst))
-# print(lst)
+test1=[('(', ')', '(', '(', ')', ')'), (')', '(', '(', '(', ')', ')'), (')', ')', '(', '(', '(', ')'), (')', '(', 
+'(', ')', ')', '('), ('(', ')', '(', ')', '(', ')'), ('(', '(', ')', ')', ')', '('), (')', ')', '(', ')', '(', '('), (')', '(', ')', '(', ')', '('), ('(', '(', ')', '(', ')', ')'), ('(', ')', ')', '(', ')', '('), (')', '(', '(', ')', '(', ')'), (')', ')', '(', '(', ')', '('), (')', ')', ')', '(', '(', '('), (')', '(', ')', ')', '(', '('), ('(', ')', ')', ')', '(', '('), ('(', ')', '(', ')', ')', '('), ('(', '(', ')', ')', '(', ')'), (')', '(', ')', '(', '(', ')'), ('(', ')', ')', '(', '(', ')'), ('(', '(', '(', ')', ')', ')')]  
+test=list(set(test1))
+result=[]
+index=[]
+for i in range(len(test)):
+    stack=[]
+    for j in test[i]:
+        if j ==')':
+            try:
+                stack.pop()
+            except:
+                index.append(i)
+                break
+        else:
+            stack.append(j)
+
+    if i not in index:
+        result.append(''.join(test[i]))
     
-# sum=0
-# for i in lst:
-#     sum += i**2
-# print(sum%int(m))
-# from itertools import product as pr
-# a=input().split()
-# k=int(a[0])
-# m=int(a[1])
-
-# N=[]
-# for i in range (k):
-#     lst=input().split()
-#     lst=[int(n) for n in lst]
-#     lst=lst[1:]
-#     N.append(lst)
-# print(N)
-    
-# product=list(pr(*N))
-# # print(pro)
-# # print(len(pro))
-# fin=0
-# for item in product:
-#     sum=0
-#     for num in item: 
-#         sum+= num**2
-#     mod = sum % k
-# if mod<fin:
-#     mod = fin        
-# print(fin)
-
-
-
-
-import itertools as it
-
-# Generate all combinations of numbers in the range 10 taken 3 at a time
-print(*list(it.combinations(range(5), 3)))
-
+print(result)
