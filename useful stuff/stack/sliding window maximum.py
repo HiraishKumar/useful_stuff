19a619ae95070045133f998c4a152c7d6b65c7a2
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-08-28 22:16:17 +0530

    stuff

--- useful stuff/stack/sliding window maximum.py
+++ useful stuff/stack/sliding window maximum.py
@@ -1,9 +1,22 @@
-test=[1,2,3,4,5,6,7,8,9]
-window=4
+test=[1,3,-1,-3,5,3,6,7]
+window=3
 
 def SlidingWindowMax(nums:list[int],k:int)->list[int]:
-    stack=nums[0:k-1]
-    results=[max(stack)]
-    for i in range(k,len(stack)):
+    stack=nums[:k-1]
+    results=[]
+    for i in nums[k-1:]:
+        if len(stack)==k:
+            stack=stack[1:]
+        while stack and stack[-1] < i:
+            stack.pop()
+        stack.append(i)
+        results.append(max(stack))
+        
+    return results
+
+    
+print(SlidingWindowMax(test,window))
+
+   
         
     
