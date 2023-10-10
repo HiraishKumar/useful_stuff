cb4ff6896e3334155072246b01443ef2c096443d
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-10-10 22:43:56 +0530

    god damn it

--- useful stuff/stack/infix evaluation.py
+++ useful stuff/stack/infix evaluation.py
@@ -1,10 +1,40 @@
-string='4*5+6/3'
+string='1-(     -2)'
 
-def infix(string:str)->int:
+def infix(string:str)->int: 
     stack=[]
-    operator=[]
+    if '+' not in string and '-' not in string:
+        ans=''
+        for i in string:
+            if i!='('and i!=')':
+                ans+=i
+        return int(ans)
+           
     for i in string:
-        pass
+        if i ==')':
+            while stack[-2]!='(':                
+                A=int(stack.pop())
+                opp=stack.pop()
+                B=int(stack.pop())
+                if opp=='+':
+                    stack.append(str(A+B))
+                elif opp=='-':
+                    stack.append(str(B-A))
+            num=stack.pop()
+            stack.pop()
+            stack.append(num)
+        elif i!=' ':
+            stack.append(i)
+    stack=stack[::-1] 
+    while len(stack)!=1:
+        A=int(stack.pop())
+        opp=stack.pop()
+        B=int(stack.pop())
+        if opp=='+':
+            stack.append(str(A+B))
+        elif opp=='-':
+            stack.append(str(A-B))
+    return int(stack[0])
+    
     
                 
 
