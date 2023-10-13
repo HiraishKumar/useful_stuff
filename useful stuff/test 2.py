efe8747ba0359bc11485cd46e1cb784b9a8be978
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-10-14 00:00:41 +0530

    sdfv

--- useful stuff/test 2.py
+++ useful stuff/test 2.py
@@ -1,26 +1,3 @@
-from tkinter import *
+from itertools import permutations
 
-root = Tk()
-
-name = StringVar()
-check_box_list = []
-ent=Entry(root,textvariable=name).pack()
-
-def clear():
-    for i in check_box_list:
-        i.pack_forget()    # forget checkbutton
-        # i.destroy()        # use destroy if you dont need those checkbuttons in future
-
-def generate():
-    k=name.get()
-    c=Checkbutton(root,text=k)
-    c.pack()
-    check_box_list.append(c)  # add checkbutton
-
-btn1=Button(root,text="Submit",command=generate)
-btn1.pack()
-
-btn2=Button(root,text="Clear",command=clear)
-btn2.pack()
-
-mainloop()
+print(list(permutations('abc')))
