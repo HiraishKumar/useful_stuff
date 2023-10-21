dd7b68a7b395b14a4012a8c9147970cfd174d39f
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-10-21 21:47:55 +0530

    stuff

--- useful stuff/test 2.py
+++ useful stuff/test 2.py
@@ -1,2 +1,10 @@
+import math
 
-print({chr(97 + i): 0 for i in range(26)}  )
+# Your float value
+float_number = 5.7
+
+# Round up to the nearest integer
+rounded_up = -int(-float_number // 1)
+
+# Display the result
+print(rounded_up)
