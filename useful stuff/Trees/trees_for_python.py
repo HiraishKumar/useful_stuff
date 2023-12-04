5c3ad1d4de7ed11b53ea276e023c61c439b8ddc2
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-12-04 19:17:09 +0530

    Trees

---
+++ useful stuff/Trees/trees_for_python.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,value):
+        self.right=None
+        self.left=None
+        self.data=value
+        
+class Tree:
+    def createNode(self,data):
+        return Node(data)
+    def AddLeft(self,data):
+        self.left = Node(data)
+    def AddRight(self,data):
+        self.right =Node(data)
+    def find (self,Node,value):
+        while True:
+            if Node.data == value:
+                return Node
+            else:
+                if Node.left and Node.left.data >= value:
+                    Node=Node.left
+                elif Node.right:
+                    Node=Node.right 
+                else:
+                    return None               
+        
