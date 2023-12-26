9b83e443f22b4c165974c6c597d32728ec1c965f
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-12-26 12:00:32 +0530

    SMOKING THAT TREE

---
+++ useful stuff/Trees/BalancedBinaryTree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: [TreeNode]) -> bool:
+        if root is None:
+            return True  # An empty tree is considered balanced
+
+        # Check the balance condition
+        if abs(self.height(root.left) - self.height(root.right)) > 1:
+            return False
+
+        # Recursively check the balance for left and right subtrees
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    
+    def height(self, root:[TreeNode])-> int:
+        if not root:
+            return 0
+        return 1 + max(self.height(root.left),self.height(root.right))
