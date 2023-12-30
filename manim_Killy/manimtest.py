4933c1711613f6a014e767bd973962e3f1ab0e33
Killy <87876086+KillyDaviel@users.noreply.github.com>
2023-12-30 14:43:29 +0530

    Stuff

--- manim_Killy/manimtest.py
+++ manim_Killy/manimtest.py
@@ -1,16 +1,33 @@
 from manim import *
-
-# class NamesOfAnimation(Scene):
-#     def construct(self):
-#         box=Square(stroke_color = GREEN, stroke_opacity=1.0,fill_color=RED ,fill_opacity=1.0,side_length=2)
-#         self.add(box)
 class MonteCarlo(Scene):
     def construct(self):
-        number_plane = NumberPlane(
-            background_line_style={
-                "stroke_color": BLUE,
-                "stroke_width": 4,
-                "stroke_opacity": 0.6
-            }
+        equation=MathTex(
+            "\partial f(x+h,y+k)","=",
+            "hf_{x}(x,y)+kf_{y}(x,y)","=",
+            "f(x+h,y+k)","-","f(x,y)"
+        ).move_to(UP*2.5).scale(0.8)
+        self.play(Write(equation))
+        framebox1 = SurroundingRectangle(equation[4:7], buff = .1)
+        framebox2 = SurroundingRectangle(equation[6], buff = .1)
+        self.play(
+            Create(framebox1),
         )
-        self.add(number_plane)       
+        zero=MathTex("0").move_to(UP*2.5+RIGHT*5).scale(0.8)
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
+        self.wait()  
+        text=Tex("By choice of function f(x,y) at (0,0) is 0").move_to(UP*2).scale(0.8)
+        self.play(Write(text))
+        self.play(ReplacementTransform(equation[6],zero))
+        self.wait()  
+        self.play(text.animate.move_to(DOWN*3.5))  
+        self.wait()
+        equation2=MathTex("\partial f(x+h,y+k)","=",
+            "hf_{x}(x,y)+kf_{y}(x,y)","=",
+            "f(x+h,y+k)")
+        self.play(Write(equation2))
+        self.play(equation2.animate.move_to(UP*2).scale(0.8))
+        self.wait()
+           
