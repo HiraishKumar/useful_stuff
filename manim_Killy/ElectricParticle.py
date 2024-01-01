33690f1674ec7195d210d895c5c6ed4bca4b22b3
Killy <87876086+KillyDaviel@users.noreply.github.com>
2024-01-01 23:27:30 +0530

    ElectricParticle

--- manim_Killy/ElectricParticle.py
+++ manim_Killy/ElectricParticle.py
@@ -2,6 +2,7 @@ from manim import *
 
 class ElectricParticle(Scene):
     def construct(self):
+        #Adds the grid onto number plane
         number_plane = NumberPlane(  
             x_range=[-10, 10, 1],
             y_range=[-10, 10, 1],
@@ -17,22 +18,30 @@ class ElectricParticle(Scene):
             },
         )
         self.add(number_plane)
+        
+        #function to determine the slope in 
+        #the x,y direction for a point(x,y)
         def Slope(pos):
+            '''pos parameter has pos[0]=x and pos[1]=y
+                        
+            function to determine the slope in 
+            the x,y direction for a point(x,y)'''
             test=(np.power(pos[0],2)+np.power(pos[1],2))
             if test!=0:
                 denom=np.power(test,5/2)
                 return (10*pos[0]/denom)*RIGHT + (10*pos[1]/denom)*UP
             else:
-                return 0*UP +0*RIGHT
-        
-        mob=StreamLines(Slope)
+                return 0*UP +0*RIGHT       
+            
+        #Adds Text Onto Screen
         text=Tex("Testing the electric field equation").scale(0.8).move_to(UP*3+RIGHT*4)
-        self.add(mob)
-        self.add(MathTex(r"\vec{E}=\frac{Q\left ( x\hat{i}+y\hat{j} \right )}{4\pi\epsilon_{0}\left ( x^{2}+y^{2} \right )^{\frac{3}{2}}}").next_to(text,DOWN*1).scale(0.8))
-        self.add(Circle(
-            radius=0.75,fill_opacity=1, fill_color=BLUE, stroke_color=WHITE
-                ))
-        self.add(MathTex("+").scale(2.5))
         self.add(text)
-        mob.start_animation(time_width=1)
-        self.wait(1)
+        self.add(MathTex(r"\vec{E}=\frac{Q\left ( x\hat{i}+y\hat{j} \right )}{4\pi\epsilon_{0}\left ( x^{2}+y^{2} \right )^{\frac{3}{2}}}").next_to(text,DOWN*1).scale(0.8))
+        
+        # creates the arrows that indicate direction
+        # and magnitude of electric Feild
+        mob=ArrowVectorField(Slope)
+        self.add(mob)
+        
+        #adds Proton To Image
+        self.add(LabeledDot(Tex("+", color=WHITE), color=BLUE).scale(2) )
