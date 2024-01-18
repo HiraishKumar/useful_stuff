ca9c0f1d9e0791af94489f31dd580ac5f0e807e9
Killy <87876086+KillyDaviel@users.noreply.github.com>
2024-01-18 21:37:57 +0530

    hackerank

--- Trees/ValidBST.py
+++ Trees/ValidBST.py
@@ -4,8 +4,7 @@ class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
-        self.right = right    
-        
+        self.right = right           
         
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
