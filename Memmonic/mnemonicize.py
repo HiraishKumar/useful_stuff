f2e2b388a40aa58109fb4ac01997cf7b1c01071d
HiraishKumar <heraishkumar123@gmail.com>
2025-03-04 22:24:05 +0530

    implemented addtion of theme in the output string

--- Memmonic/mnemonicize.py
+++ Memmonic/mnemonicize.py
@@ -2,7 +2,12 @@ from random import shuffle
 from constants import OB_mnemonics
 
 class mnemonicize:
-    def __init__(self,mnem_dic):
+    """Takes in a Dict input in the format { "PMSHFR": "Perception Error" } and produces
+    a randomized sequence and list of the indexes of the mnemonic for rivision pourposes,
+    in the format ""[3, 1, 6, 2, 5, 4]        | PMSHFR  | Perception Error""
+    where the key is the first letters of the mnemoinic in all caps no spaces
+    and the value is the theme of the mnemonic"""
+    def __init__(self,mnem_dic:dict):
         self.mnem_dic = mnem_dic
 
     def unique_numbers_string(start:int,end:int)->list[int]:
@@ -27,10 +32,10 @@ class mnemonicize:
             print_str_array = []
             if index:
                 print_str_array.append(f"{_index:<25}") 
+            if mnemonic:
+                print_str_array.append(f"{mnem_keys[key_index]:7}")
             if theme:
                 print_str_array.append(f"{self.mnem_dic[mnem_keys[key_index]]:<40}")
-            if mnemonic:
-                ...
             # formatted_output = f"{_index:<25} | {key_index:<1} | {self.mnem_dic[mnem_keys[key_index]]:<40}"
             # print(formatted_output)
             print_str = " | ".join(print_str_array)
